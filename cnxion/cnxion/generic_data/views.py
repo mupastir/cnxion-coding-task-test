9ee0a8b2e04fc9c999cffc04b6116b2b6a05d9d0
oleg <mupastir@gmail.com>
2019-12-13 10:14:45 +0200

    add model field in views

--- cnxion/cnxion/generic_data/views.py
+++ cnxion/cnxion/generic_data/views.py
@@ -1,7 +1,13 @@
 from django.views.generic import CreateView
 
 from cnxion.generic_data.forms import GenericUniversalForm
+from cnxion.generic_data.models import GenericModel
 
 
 class GenericCreateView(CreateView):
+
+    model = GenericModel
     form_class = GenericUniversalForm
+
+
+generic_create_view = GenericCreateView.as_view()
