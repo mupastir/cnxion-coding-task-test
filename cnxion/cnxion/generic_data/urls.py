e2e7dc1d71301f4fe50acb5149a891c04c80d489
oleg <mupastir@gmail.com>
2019-12-13 10:14:21 +0200

    change view call style

--- cnxion/cnxion/generic_data/urls.py
+++ cnxion/cnxion/generic_data/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from cnxion.generic_data.views import GenericCreateView
+from cnxion.generic_data.views import generic_create_view
 
 app_name = "generic_data"
 urlpatterns = [
-    path("~add-data/", view=GenericCreateView.as_view(), name="create"),
+    path("~add/", view=generic_create_view, name="create"),
 ]
