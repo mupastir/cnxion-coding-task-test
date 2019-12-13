35f6317e5ba4266271ef26299268dd40a44e1865
oleg <mupastir@gmail.com>
2019-12-13 16:24:39 +0200

    modified models representation in admin

--- cnxion/cnxion/generic_data/models.py
+++ cnxion/cnxion/generic_data/models.py
@@ -4,3 +4,6 @@ from django_extensions.db.fields.json import JSONField
 
 class GenericModel(models.Model):
     generic_data = JSONField()
+
+    def __str__(self):
+        return str(self.generic_data)
